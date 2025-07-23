4b7728ed359b6f6b7db1fc68854a7019e5054092
rohit <rp2012946@gmail.com>
2025-07-23 10:13:35 +0530

    Deploy both backend and frontend together

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ const cors = require('cors');
 const colors = require('colors');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
+const path = require('path');
 const connectDb = require('./config/connectDb');
 
-// config dotenv file 
+// config dotenv
 dotenv.config();
-
 
 // database call
 connectDb();
 
-// rest object 
+// rest object
 const app = express();
 
 // Global error handler
@@ -21,21 +21,31 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-// middlewares 
+// middlewares
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 
-// user routes 
+// routes
 app.use('/api/v1/users', require('./routes/userRoute'));
-
-// transaction routes
 app.use('/api/v1/transactions', require('./routes/transactionRoutes'));
 
+// ======================
+// Serve React frontend
+// ======================
+const __dirname1 = path.resolve(); // resolve current path
+app.use(express.static(path.join(__dirname1, 'client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname1, 'client', 'build', 'index.html'));
+});
+
 // port
-const PORT = 8080 || process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 // listen
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server Running On ${PORT}`);
 });
+
+
